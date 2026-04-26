42e51a5c54771fd6b682e262d53fce4003f492ae
VENU MADHAV <venumadhav742003@gmail.com>
2026-04-26 15:43:46 +0530

    site map fixed-final-2

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import PlainTextResponse
+from fastapi.responses import PlainTextResponse, Response
 from fastapi.staticfiles import StaticFiles
 
 from app.routers import emi, home, legal, overlap, sip, tax
@@ -17,28 +17,32 @@ app = FastAPI(
     redoc_url=None,
 )
 
+# ✅ CLEAN SITEMAP (FIXED)
 @app.get("/sitemap.xml", include_in_schema=False)
 async def sitemap():
-    #return PlainTextResponse(
-        content='<?xml version="1.0" encoding="UTF-8"?>'
-        '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'
-        '<url><loc>https://getfincalx.com/</loc></url>'
-        '<url><loc>https://getfincalx.com/tools/sip-calculator</loc></url>'
-        '<url><loc>https://getfincalx.com/tools/emi-calculator</loc></url>'
-        '<url><loc>https://getfincalx.com/tools/income-tax-calculator</loc></url>'
-        '<url><loc>https://getfincalx.com/tools/portfolio-overlap-checker</loc></url>'
-        '</urlset>',
-        media_type="application/xml"
-    #)
+    xml_content = """<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url><loc>https://getfincalx.com/</loc></url>
+    <url><loc>https://getfincalx.com/tools/sip-calculator</loc></url>
+    <url><loc>https://getfincalx.com/tools/emi-calculator</loc></url>
+    <url><loc>https://getfincalx.com/tools/income-tax-calculator</loc></url>
+    <url><loc>https://getfincalx.com/tools/portfolio-overlap-checker</loc></url>
+</urlset>
+"""
+    return Response(content=xml_content, media_type="application/xml")
 
+
+# ✅ ROBOTS.TXT
 @app.get("/robots.txt", include_in_schema=False)
 async def robots():
     return PlainTextResponse(
         content="User-agent: *\n"
                 "Allow: /\n"
-                "Sitemap: https://getfincalx.com/sitemap.xml",
+                "Sitemap: https://getfincalx.com/sitemap.xml"
     )
 
+
+# ✅ MIDDLEWARES
 app.add_middleware(SecurityHeadersMiddleware)
 app.add_middleware(RateLimitMiddleware, max_requests=100, window_seconds=60)
 app.add_middleware(
@@ -49,12 +53,13 @@ app.add_middleware(
     allow_headers=["Content-Type"],
 )
 
+# ✅ STATIC FILES
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
+# ✅ ROUTERS
 app.include_router(home.router)
 app.include_router(sip.router)
 app.include_router(emi.router)
 app.include_router(tax.router)
 app.include_router(overlap.router)
 app.include_router(legal.router)
-
